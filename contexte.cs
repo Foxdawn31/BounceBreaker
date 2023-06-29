460d171e7c62d6c0db6b35c044ed9233310a1919
Foxdawn31 <yod1001@hotmail.com>
2023-06-29 17:32:52 +0200

    vies et score v1

---
+++ contexte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BounceBreaker
+{
+    public static class Contexte
+    {
+        static int Score;
+        static int Lives;
+
+        static Contexte()
+        {
+            Score = 0;
+            Lives = 4 ;
+        }
+    }
+}
