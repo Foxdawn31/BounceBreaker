2e6ce0ad1f7070482137e62c006000608e34df40
Foxdawn31 <yod1001@hotmail.com>
2023-06-30 02:15:47 +0200

    heh

---
+++ Contexte.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BounceBreaker
+{
+    public static class Contexte
+    {
+        public static int score { get; private set; }
+        static int Lives;
+
+        public static int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+                if (score > 1500)
+                    Lives++;
+
+            }
+        }
+
+        static Contexte()
+        {
+            score = 0;
+            Lives = 4 ;
+        }
+    }
+}
