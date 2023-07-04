01db2a70991421e422bef2e2e0f4f888acd9ad46
Foxdawn31 <yod1001@hotmail.com>
2023-07-04 13:44:42 +0200

    V 1.3 (SPOO jour 2)

--- SceneGameplay.cs
+++ SceneGameplay.cs
@@ -11,41 +11,41 @@ namespace BounceBreaker
 {
     class SceneGameplay : Scene
     {
-        Texture2D TexPad;
-        int Pad_x;
-        int Pad_y;
-
-        Texture2D TexBall;
-        int Ball_x;
-        int Ball_y;
+       
+        Sprites SprPad;
+        Sprites SprBall;
+       
 
         public SceneGameplay(Game Pgame) : base(Pgame)
         {
-            Pad_x = 10;
-            Pad_y = 450;
-            Ball_x = 40;
-            Ball_y = 50;
+            
+          
 
-            TexPad = Pgame.Content.Load<Texture2D>("Pad");
-            TexBall = Pgame.Content.Load<Texture2D>("Ball");
+            Contexte.lives = 5;
+
+         
+            SprPad = new Sprites(Pgame.Content.Load<Texture2D>("Pad"));
+            SprPad.SetPosition (new Vector2 (10, 450));
+            SprBall = new Sprites(Pgame.Content.Load<Texture2D>("Ball"));
+          //SprBall.Setposition 
         }
         public override void Update()
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
-                Pad_x = Pad_x + 3;
+            //    Pad_x = Pad_x + 3;
             }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Left) && Pad_x >= 0)
-            {
-                Pad_x = Pad_x - 3;
-            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+             {
+            //    Pad_x = Pad_x - 3;
+             }
+
         }
         public override void Draw(SpriteBatch pBatch)
         {
-            pBatch.Draw(TexPad, new Vector2(Pad_x, Pad_y), Color.White);
-            pBatch.Draw(TexBall, new Vector2(Ball_x, Ball_y), Color.White);
-
-            base.Draw(pBatch);
+        base.Draw(pBatch);
+                SprPad.Draw(pBatch);
+                SprBall.Draw(pBatch);
         }
     }
 }
---
+++ Sprites.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Sprites
+    {
+    public Vector2 Position { get; set; }
+    public Vector2 Speed { get; set; }
+    private Texture2D Texture;
+        public Sprites(Texture2D pTexture) 
+        { 
+        Texture = pTexture;
+        }
+
+    public void SetPosition(Vector2 pPosition)
+    {
+        Position = pPosition;
+    }
+
+        public void Draw(SpriteBatch pBatch) 
+        {
+            pBatch.Draw(Texture, Position,Color.White);
+        }
+
+    }
